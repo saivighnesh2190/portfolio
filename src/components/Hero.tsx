f05af248e4f89e763fa0963770e4d8d45444a093
saivighnesh2190 <2300030455@kluniversity.in>
2025-10-21 11:46:47 +0530

    first commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,19 +4,7 @@ import { BackgroundCanvas } from './BackgroundCanvas'
 
 export function Hero() {
   const [linkedIn] = hero.socials
-
-  const downloadResume = () => {
-    const pdfContent = `%PDF-1.4\n1 0 obj\n<<\n/Type /Catalog\n/Pages 2 0 R\n>>\nendobj\n2 0 obj\n<<\n/Type /Pages\n/Kids [3 0 R]\n/Count 1\n>>\nendobj\n3 0 obj\n<<\n/Type /Page\n/Parent 2 0 R\n/MediaBox [0 0 612 792]\n/Contents 4 0 R\n/Resources << /Font << /F1 5 0 R >> >>\n>>\nendobj\n4 0 obj\n<<\n/Length 120\n>>\nstream\nBT\n/F1 16 Tf\n90 720 Td\n(${hero.name} Resume Snapshot) Tj\n/F1 12 Tf\n0 -28 Td\n(${hero.title}) Tj\n0 -22 Td\n(Email: ${hero.email}) Tj\n0 -22 Td\n(Location: ${hero.location}) Tj\n0 -22 Td\n(Phone: ${hero.phone}) Tj\nET\nendstream\nendobj\n5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\nxref\n0 6\n0000000000 65535 f \n0000000010 00000 n \n0000000061 00000 n \n0000000124 00000 n \n0000000250 00000 n \n0000000409 00000 n \ntrailer\n<<\n/Size 6\n/Root 1 0 R\n>>\nstartxref\n481\n%%EOF`
-    const blob = new Blob([pdfContent], { type: 'application/pdf' })
-    const url = URL.createObjectURL(blob)
-    const link = document.createElement('a')
-    link.href = url
-    link.download = hero.resumeFileName
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-    URL.revokeObjectURL(url)
-  }
+  const resumeUrl = `${import.meta.env.BASE_URL}${hero.resumeFileName}`
 
   const handleContactScroll = () => {
     const target = document.getElementById('contact')
@@ -38,10 +26,10 @@ export function Hero() {
           <button className="button button--primary" type="button" onClick={handleContactScroll}>
             Let&apos;s collaborate
           </button>
-          <button className="button button--secondary" type="button" onClick={downloadResume}>
+          <a className="button button--secondary" href={resumeUrl} download>
             Download resume
             <Download size={18} />
-          </button>
+          </a>
           {linkedIn ? (
             <a className="button button--secondary" href={linkedIn.url} target="_blank" rel="noreferrer">
               Connect on {linkedIn.label}
